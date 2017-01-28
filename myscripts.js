411ecf7554bad243eb826b37dc2563f275e9eacd
thegracejkim <thegracejkim@gmail.com>
2017-01-28 13:09:34 -0500

    make fixes

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,11 @@
     function changeColor(){
       document.getElementById("content").style.color="black";
     }
+var operand1;
+var operand2;
+
+function getNumbers(){
+  operand1 = Math.floor(Math.random() * 20) + 1;
+  operand2 = Math.floor(Math.random() * 20) + 1;
+  document.getElementById("operand1").
+}

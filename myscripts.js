422548993fce775e1f030bbf35d7db1d9bf04a01
thegracejkim <thegracejkim@gmail.com>
2017-02-04 12:55:35 -0500

    did some stuff

--- myscripts.js
+++ myscripts.js
@@ -23,4 +23,25 @@ function checkAnswer(){
     document.getElementById("results").innerHTML="Incorrect.";
   }
 }
-var pics
+//Gallery
+var pics = ["https://s-media-cache-ak0.pinimg.com/736x/0a/b8/01/0ab8016b4307a437df59eb935105668c.jpg",
+           "https://s-media-cache-ak0.pinimg.com/originals/96/f3/07/96f307e172421ddaa7cd7e8e7e763db2.jpg",
+           "https://s-media-cache-ak0.pinimg.com/originals/89/54/a1/8954a1e6c0ec70b5e02518cd6e8ec75d.jpg"
+           ];
+
+var picIndex = 0;
+
+function goLeft(){
+  if(picInex > 0 ){
+    picIndex = picIndex - 1;
+    document.getElementById("pic").src=pics[picIndex];
+  }
+  
+}//end of function
+
+function goRight(){
+  if(picIndex < (pics.length - 1)){
+    picIndex = picIndex + 1;
+    document.getElementById("pic").src=pics[picIndex];
+  }
+}
